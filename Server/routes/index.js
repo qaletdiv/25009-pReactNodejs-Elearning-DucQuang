903a60f511894f74bb95a82212d9568d5d83494e
QuangNgủYên <116800555+quangne114@users.noreply.github.com>
2025-05-13 02:00:23 +0700

    Delete Server directory

--- Server/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express'); 
-const rootRouter = express.Router(); 
-const userRouter = require('./userRoute')
-
-rootRouter.use('/users', userRouter)
-
-module.exports = rootRouter
--- Server/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express'); 
-const userRouter = express.Router(); 
-const userController = require('../controllers/userController')
-const {registerValidationRule} = require('../validations/userValidator')
-const validationErrorHandler = require('../middlewares/ValidationErrorHanlder/validationErrorHandler')
-
-userRouter.post('/register',registerValidationRule(), validationErrorHandler, userController.createUser)
-
-module.exports = userRouter
