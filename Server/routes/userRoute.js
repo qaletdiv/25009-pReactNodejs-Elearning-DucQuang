1b16bbf6eaf58c52ff6261cb5df184c902b4af49
quang114 <quang025114@gmail.com>
2025-05-13 18:05:55 +0700

    Add login functionality and fix email validation in registration

--- Server/routes/userRoute.js
+++ Server/routes/userRoute.js
@@ -1,9 +1,11 @@
 const express = require('express'); 
 const userRouter = express.Router(); 
 const userController = require('../controllers/userController')
-const {registerValidationRule} = require('../validations/userValidator')
+const {registerValidationRule, loginValidationRule} = require('../validations/userValidator')
 const validationErrorHandler = require('../middlewares/ValidationErrorHanlder/validationErrorHandler')
 
+
 userRouter.post('/register',registerValidationRule(), validationErrorHandler, userController.createUser)
+userRouter.post('/login', loginValidationRule(), validationErrorHandler, userController.login )
 
 module.exports = userRouter
